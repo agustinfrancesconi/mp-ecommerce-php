f9fd28979224ebe660a2518b59b8891c1c2c18fa
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-09-10 23:03:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
                                     <img src="./assets/music-audio-alp-201709" alt="" width="1440" height="320" data-scale-params-2="wid=2880&amp;hei=640&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1503948581306" class="pd-billboard-hero ir">
                                 </div>
                                 <div class="pd-billboard-info">
-                                    <h1 class="pd-billboard-header pd-util-compact-small-18">Tienda e-commerce</h1>
+                                    <h1 class="pd-billboard-header pd-util-compact-small-18">CelTech e-commerce</h1>
                                 </div>
                             </div>
                         </div>

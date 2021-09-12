fd96618f3d81c1ae70fb8fab1d1f6ab027f22228
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2021-09-12 19:02:20 -0300

    removendo bug na pagina de compra

--- detail.php
+++ detail.php
@@ -124,11 +124,11 @@
                                             </h3>
                                         </div>
                                         <h3 >
-                                            <?php echo $_POST['price'] ?>
+                                            <?php echo "$" . $_POST['price'] ?>
                                         </h3>
-                                        <h3 >
-                                            <?php echo "$" . $_POST['unit'] ?>
-                                        </h3>
+                                        <!-- <h3 >
+                                            <?php // echo "$" . $_POST['unit'] ?>
+                                        </h3> -->
                                     </div>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
                                 </div>
